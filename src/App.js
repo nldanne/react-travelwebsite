fb6bc284f769abff93298cac71c02232829141a0
nldanne <nldanne97@gmail.com>
2021-12-29 16:06:02 +0100

    navlinks with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
