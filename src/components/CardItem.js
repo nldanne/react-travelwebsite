abedda7c947c36575003432bf499d356f73169b1
nldanne <nldanne97@gmail.com>
2022-02-14 09:45:44 +0100

    created template for Cards

---
+++ src/components/CardItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// props will be coming from Cards.js
+function CardItem(props) {
+  return (
+    <>
+      <li className='cards__item'>
+        <Link to={props.path} className='cards__item__link'>
+          <figure data-category={props.label} className='cards__item__pic-wrap'>
+            <img src={`images/${props.src}`} alt="Travel Image" className='cards__item__img'></img>
+          </figure>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{props.text}</h5>
+          </div>
+        </Link>
+      </li>
+    </>
+  )
+}
+
+export default CardItem;
