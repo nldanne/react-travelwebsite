7a61216d2499ed0851e6b7fb165da3e25121e38d
nldanne <nldanne97@gmail.com>
2022-02-14 09:41:30 +0100

    setup navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 import './App.css';
 
 function App() {
@@ -9,7 +13,10 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' exact />
+        <Route path='/' exact element={<Home />} />
+        <Route path='/services' exact element={<Services />} />
+        <Route path='/products' exact element={<Products />} />
+        <Route path='/sign-up' exact element={<SignUp />} />
       </Routes>
     </BrowserRouter>
     </>
