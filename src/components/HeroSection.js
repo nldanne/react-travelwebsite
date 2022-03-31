9513f227276a4b5b2ab21fc7805770a2596f3250
nldanne <nldanne97@gmail.com>
2022-03-31 18:34:26 +0200

    added search form and routeChange

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,31 +1,51 @@
 import React from 'react'
+
+import { useNavigate } from 'react-router-dom';
 import '../App.css';
-import { Button } from './Button';
 import './HeroSection.css';
 
 function HeroSection() {
+  let navigate = useNavigate();
+
+  const routeChange = () => { 
+    let path = '/services'; 
+    navigate(path);
+  }
+
+
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
 
-      <div className='hero-btns'>
-        <Button 
-        className='btns' 
-        buttonStyle='btn--outline' 
-        buttonSize='btn--large'
-        >
-          GET STARTED
-        </Button>
-        <Button 
-        className='btns' 
-        buttonStyle='btn--primary' 
-        buttonSize='btn--large'
-        >
-          WATCH TRAILER <i className='far fa-play-circle'></i>
-        </Button>
-      </div>
+      <form className='search'>
+          <div className='search-container'>
+            <label >Where are you going?</label>
+            <input id='location' type='text' placeholder='Search your location' />
+          </div>
+          <div className='row-container'>
+
+            <div className='search-container'>
+              <label>Check in</label>
+              <input id='check-in' type='date' />
+            </div>
+            <div className='search-container'>
+              <label>Check out</label>
+              <input id='check-out' type='date' />
+            </div>
+
+          </div>
+          <div className='search-container'>
+              <button
+                className='hero-btn'
+                type='submit'
+                onClick={routeChange}
+              >
+                Explore
+              </button>
+          </div>
+      </form>
     </div>
   )
 }
