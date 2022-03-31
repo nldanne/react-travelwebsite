5020c7c1f7c64f5e98676c4fdaec7516e3f1a2c6
nldanne <nldanne97@gmail.com>
2022-03-31 18:35:01 +0200

    updated routes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
-import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import Destination from './components/pages/Destination';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path='/services' exact element={<Services />} />
-        <Route path='/products' exact element={<Products />} />
-        <Route path='/sign-up' exact element={<SignUp />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/services' exact element={<Services />} />
+          <Route path='/sign-up' exact element={<SignUp />} />
+          <Route path='/services/activity' element={<Destination />} />
+        </Routes>
+      </ScrollToTop>
     </div>
   );
 }
