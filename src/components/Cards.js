804c2b664838d3e16b48225213eaf954f9e8b4c9
nldanne <nldanne97@gmail.com>
2022-03-31 18:32:43 +0200

    updated path names

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,13 +13,13 @@ function Cards() {
             src="img-9.jpg"
             text="Explore the hidden waterfall deep inside the Amazon Jungle"
             label="Adventure"
-            path="/services"
+            path="/services/activity"
             ></CardItem>
             <CardItem 
             src="img-2.jpg"
             text="Travel through the Islands of Bali in a Private Cruise"
             label="Luxury"
-            path="/services"
+            path="/services/activity"
             ></CardItem>
           </ul>
           <ul className='cards__items'>
@@ -27,19 +27,19 @@ function Cards() {
             src="img-4.jpg"
             text="Experience Football on Top of the Himalayan Mountains"
             label="Adventure"
-            path="/services"
+            path="/services/activity"
             ></CardItem>
             <CardItem 
             src="img-3.jpg"
             text="Set a Sail in the Atlantic ocean visiting Uncharted Waters"
             label="Mystery"
-            path="/services"
+            path="/services/activity"
             ></CardItem>
             <CardItem 
             src="img-8.jpg"
             text="Ride through the Sahara Desert on a guided camel tour"
             label="Adrenaline"
-            path="/services"
+            path="/services/activity"
             ></CardItem>
           </ul>
         </div>
