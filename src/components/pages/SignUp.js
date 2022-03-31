9df16a60c6d3237761f10c45a32c427ea752fca9
nldanne <nldanne97@gmail.com>
2022-03-31 18:35:50 +0200

    created a sign up form

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -3,5 +3,28 @@ import '../../App.css';
 
 
 export default function SignUp() {
-  return <h1 className='sign-up'>SIGN UP</h1>;
+  return (
+    <div className='form-modal__container'>
+      <div className='form-modal__wrapper' >
+        <div className='sign-up'>
+          <img src='/images/img-8.jpg' alt='Camels in the desert'></img>
+        </div>
+        <div className='sign-up__container'>
+          <h2>Sign Up</h2>
+          <form className='sign-up__form'>
+            <label>Email</label> <br></br>
+            <input type='text' placeholder='johndoe@gmail.com'></input><br></br>
+            <label>Password</label> <br></br>
+            <input type='password' placeholder='password'></input><br></br>
+            <button type='submit' className='btn-sign'>Sign Up</button>
+          </form>
+
+          <div>
+            <p className='have-account'>Have an account? <span>Log In here </span></p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
 }
